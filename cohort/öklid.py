474755381113a46cd55966f58592d31675ca2a1d
aystskan <ays.taskan@gmail.com>
2024-08-04 22:14:35 +0300

    Add files via upload
    distance

---
+++ cohort/öklid.py
@@ -0,0 +1,17 @@
+points = [(1, 2), (3, 4), (5, 6), (7, 8)]  # Example list of points
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    return distance
+
+distances = []  # List to store calculated distances
+
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+print("Minimum distance:", min_distance)
